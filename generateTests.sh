e0d91933bb178a166beae5972b2798df7761e224
Sasha Fedchin <fedchina@amazon.com>
2022-07-29 14:44:30 -0700

    Update modules

--- generateTests.sh
+++ generateTests.sh
@@ -1,13 +1,14 @@
 for filename in $(find src/ -name '*.dfy'); do
     [ -e "$filename" ] || continue
     propername=${filename##*/}
-    echo $filename
     cd test/
-    # if [ -e "$propername" ]; then
-    #     cd ../
-    #     continue
-    # fi
-    dotnet ../dafny/Binaries/Dafny.dll /definiteAssignment:3 /warnShadowing /generateTestTimeout:7 /generateTestMode:Block /generateTestSeqLengthLimit:20 /generateTestOracle:Spec /prune ../$filename > $propername
+    if [ -e "$propername" ]; then
+        echo "Skipping file because tests exist "$filename
+        cd ../
+        continue
+    fi
+    echo $filename
+    dotnet ../dafny/Binaries/Dafny.dll /definiteAssignment:3 /warnShadowing /generateTestTimeout:7 /generateTestMode:Block /generateTestPrintStats:$propername.json /generateTestSeqLengthLimit:20 /generateTestOracle:Spec /prune ../$filename > $propername
     pkill -9 -f "z3/bin/z3"
     pkill -9 -f "local/bin/z3"
     cd ../
